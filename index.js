8402f8d67b6e1c43287c694b7b4e89997eaf0123
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2025-02-28 10:06:03 -0100

    new commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose
 
 
   app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000', 'https://food-nextjs-pearl.vercel.app' ],
     credentials: true,
   }));
 app.use(express.urlencoded({ extended: true }));
